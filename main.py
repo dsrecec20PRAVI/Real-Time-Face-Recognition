e3233927ffc0d63ff8db481d8aa7c292dbba8109
Domagoj Srečec <dodosrecec@gmail.com>
2025-01-17 17:12:10 +0100

    Made structure to capture face over a video, segment it and send it to the model for analysis

--- main.py
+++ main.py
@@ -1,13 +1,40 @@
-import argparse
-from src.train import train
-from src.evaluate import evaluate
+import threading
+import cv2
 
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    parser.add_argument("mode", choices=["train", "evaluate"], help="Run training or evaluation")
-    args = parser.parse_args()
+cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)
 
-    if args.mode == "train":
-        train()
-    elif args.mode == "evaluate":
-        evaluate()
+cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
+cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
+
+counter = 0
+face_check = False
+#Pozivanje modela
+def model_face_check(frame):
+    global face_check
+    face_check = True
+
+
+while True:
+    ret, frame = cap.read()
+
+    if ret:
+        #Mozemo uzimat vise/manje frame-ova ovisno kak nama pase
+        if counter % 30 == 0:
+            try: 
+                threading.Thread(target=model_face_check, args=(frame.copy(),)).start()
+            except ValueError:
+                pass
+        counter += 1
+
+        if face_check:
+            cv2.putText(frame, "MATCH!", (20, 450), cv2.FONT_HERSHEY_SIMPLEX, 2, (0, 255, 0), 3)
+        else:
+            cv2.putText(frame, "NO MATCH!", (20, 450), cv2.FONT_HERSHEY_SIMPLEX, 2, (0, 0, 255), 3)
+
+        cv2.imshow("video", frame)
+
+    key = cv2.waitKey(1)
+    if key == ord("q"):
+        break
+
+cv2.destroyAllWindows()
